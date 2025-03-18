2a9daa8be3f9becfafcf1ed165bcff429642f767
Giheon Do <dgh0001@naver.com>
2025-03-18 21:23:47 +0900

    feat: global하게 사용되는 기능들 추가

---
+++ src/main/java/datastreams_knu/bigpicture/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package datastreams_knu.bigpicture.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
