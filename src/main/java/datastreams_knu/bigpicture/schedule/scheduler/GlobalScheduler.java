46c037b8b63c132cc5b103a252001ee87b856a53
Giheon Do <dgh0001@naver.com>
2025-04-09 21:43:13 +0900

    chore: 주석에 시간 수정

--- src/main/java/datastreams_knu/bigpicture/schedule/scheduler/GlobalScheduler.java
+++ src/main/java/datastreams_knu/bigpicture/schedule/scheduler/GlobalScheduler.java
@@ -18,19 +18,19 @@ public class GlobalScheduler {
     }
 
     // 한국 / 미국 금리 크롤링
-    @Scheduled(cron = "0 30 1 * * MON", zone = "Asia/Seoul") // 월요일 새벽 2시
+    @Scheduled(cron = "0 30 1 * * MON", zone = "Asia/Seoul") // 월요일 새벽 1시 30분
     public void runInterestCrawling() {
         schedulerService.interestCrawling();
     }
 
     // 기본 경제 뉴스 크롤링
-    @Scheduled(cron = "0 0 2 * * MON", zone = "Asia/Seoul") // 월요일 새벽 3시
+    @Scheduled(cron = "0 0 2 * * MON", zone = "Asia/Seoul") // 월요일 새벽 2시
     public void runGeneralNewsCrawling() {
         schedulerService.generalNewsCrawling();
     }
 
     // 주가 + 관련 뉴스 크롤링
-    @Scheduled(cron = "0 30 2 * * MON", zone = "Asia/Seoul") // 월요일 새벽 4시
+    @Scheduled(cron = "0 30 2 * * MON", zone = "Asia/Seoul") // 월요일 새벽 2시 30분
     public void runNewsCrawling() {
         schedulerService.stockAndNewsCrawling();
     }
