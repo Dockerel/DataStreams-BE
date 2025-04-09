f9c97c4151c56cbe8c645406edbac3386cabbae4
Giheon Do <dgh0001@naver.com>
2025-04-09 21:20:41 +0900

    feat: globalAdviceHandler 추가

---
+++ src/main/java/datastreams_knu/bigpicture/common/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,31 @@
+package datastreams_knu.bigpicture.common.advice;
+
+import datastreams_knu.bigpicture.common.domain.ApiResponse;
+import datastreams_knu.bigpicture.common.exception.ObjectMapperException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ObjectMapperException.class)
+    public ApiResponse<Object> objectMapperException(ObjectMapperException e) {
+        return ApiResponse.of(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            e.getMessage()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> bindException(BindException e) {
+        return ApiResponse.of(
+            HttpStatus.BAD_REQUEST,
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+        );
+    }
+}
