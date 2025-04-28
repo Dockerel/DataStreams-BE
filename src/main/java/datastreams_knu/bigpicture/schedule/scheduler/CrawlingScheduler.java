e219829e70214fac17ecc4da25b080b51a98759a
Giheon Do <dgh0001@naver.com>
2025-04-28 22:07:10 +0900

    refactor: 스케줄링 시간 조정

--- src/main/java/datastreams_knu/bigpicture/schedule/scheduler/CrawlingScheduler.java
+++ src/main/java/datastreams_knu/bigpicture/schedule/scheduler/CrawlingScheduler.java
@@ -12,25 +12,25 @@ public class CrawlingScheduler {
     private final CrawlingSchedulerService schedulerService;
 
     // 달러 환율 크롤링
-    @Scheduled(cron = "0 0 1 * * MON", zone = "Asia/Seoul") // 월요일 새벽 1시
+    @Scheduled(cron = "0 10 23 * * SUN", zone = "Asia/Seoul") // 매주 일요일 오후 11시 10분
     public void runExchangeCrawling() {
         schedulerService.exchangeCrawling();
     }
 
     // 한국 / 미국 금리 크롤링
-    @Scheduled(cron = "0 30 1 1 * *", zone = "Asia/Seoul") // 매달 1일 새벽 1시 30분
+    @Scheduled(cron = "0 0 1 20 * *", zone = "Asia/Seoul") // 매달 20일 오전 1시
     public void runInterestCrawling() {
         schedulerService.interestCrawling();
     }
 
     // 기본 경제 뉴스 크롤링
-    @Scheduled(cron = "0 15 2 * * *", zone = "Asia/Seoul") // 매일 새벽 2시
+    @Scheduled(cron = "0 50 1 * * *", zone = "Asia/Seoul") // 매일 오전 1시 50분
     public void runGeneralNewsCrawling() {
         schedulerService.generalNewsCrawling();
     }
 
     // 주가 + 관련 뉴스 크롤링
-    @Scheduled(cron = "0 30 2 * * *", zone = "Asia/Seoul") // 매일 새벽 2시 30분
+    @Scheduled(cron = "0 0 2 * * *", zone = "Asia/Seoul") // 매일 새벽 2시
     public void runNewsCrawling() {
         schedulerService.stockAndNewsCrawling();
     }
--- src/main/java/datastreams_knu/bigpicture/schedule/scheduler/ExpireDataDeleteScheduler.java
+++ src/main/java/datastreams_knu/bigpicture/schedule/scheduler/ExpireDataDeleteScheduler.java
@@ -15,28 +15,28 @@ public class ExpireDataDeleteScheduler {
     private final ExpireDataDeleteSchedulerService schedulerService;
 
     // 달러 환율 크롤링 데이터 삭제
-    @Scheduled(cron = "0 10 1 * * MON", zone = "Asia/Seoul") // 매주 월요일 새벽 1시 15분
+    @Scheduled(cron = "0 20 23 * * SUN", zone = "Asia/Seoul") // 매주 일요일 오후 11시 20분
     public void deleteExchangeData() {
         DeleteResultDto result = schedulerService.deleteExpireExchangeData();
         log.info("{}: {}", result.getMessage(), result.getDeleteCount());
     }
 
     // 한국 금리 크롤링 데이터 삭제
-    @Scheduled(cron = "0 40 1 1 * *", zone = "Asia/Seoul") // 매달 1일 새벽 1시 40분
+    @Scheduled(cron = "0 20 1 20 * *", zone = "Asia/Seoul") // 매달 20일 새벽 1시 20분
     public void deleteKoreaInterestData() {
         DeleteResultDto result = schedulerService.deleteExpireKoreaInterestData();
         log.info("{}: {}", result.getMessage(), result.getDeleteCount());
     }
 
     // 미국 금리 크롤링 데이터 삭제
-    @Scheduled(cron = "0 50 1 1 * *", zone = "Asia/Seoul") // 매달 1일 새벽 1시 50분
+    @Scheduled(cron = "0 30 1 20 * *", zone = "Asia/Seoul") // 매달 20일 새벽 1시 30분
     public void deleteUSInterestData() {
         DeleteResultDto result = schedulerService.deleteExpireUSInterestData();
         log.info("{}: {}", result.getMessage(), result.getDeleteCount());
     }
 
     // 뉴스 크롤링 데이터 삭제
-    @Scheduled(cron = "0 0 2 * * *", zone = "Asia/Seoul") // 매일 새벽 2시
+    @Scheduled(cron = "0 40 1 * * *", zone = "Asia/Seoul") // 매일 새벽 1시 40분
     public void deleteNewsData() {
         DeleteResultDto result = schedulerService.deleteExpireNewsData();
         log.info("{}: {}", result.getMessage(), result.getDeleteCount());
