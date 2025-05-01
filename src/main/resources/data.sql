77fd5e75b4eacaf2978db7fc6dfd67c266548776
Giheon Do <dgh0001@naver.com>
2025-05-01 09:00:17 +0900

    fix: 변경된 필드명에 따라 sql 쿼리 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 DELETE FROM crawling_info WHERE 1;
 
-INSERT INTO crawling_info (NEWS_KEYWORD, STOCK_NAME, STOCK_TYPE) VALUES ('삼성전자', '삼성전자', 'korea');
-INSERT INTO crawling_info (NEWS_KEYWORD, STOCK_NAME, STOCK_TYPE) VALUES ('LG디스플레이', 'LG디스플레이', 'korea');
-INSERT INTO crawling_info (NEWS_KEYWORD, STOCK_NAME, STOCK_TYPE) VALUES ('애플', 'AAPL', 'us');
-INSERT INTO crawling_info (NEWS_KEYWORD, STOCK_NAME, STOCK_TYPE) VALUES ('엔비디아', 'NVDA', 'us');
+INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'korea', '삼성전자', '삼성전자');
+INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'korea', 'LG디스플레이', 'LG디스플레이');
+INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'us', 'AAPL', '애플');
+INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'us', 'NVDA', '엔비디아');
