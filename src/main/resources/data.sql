814c698ab9bd5499963a2d99aa94a66ddd6a4287
Giheon Do <dgh0001@naver.com>
2025-05-01 09:05:46 +0900

    remove: 초기 데이터 및 디버깅용 프로퍼티 삭제

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM crawling_info WHERE 1;
-
-INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'korea', '삼성전자', '삼성전자');
-INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'korea', 'LG디스플레이', 'LG디스플레이');
-INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'us', 'AAPL', '애플');
-INSERT INTO crawling_info (STOCK_TYPE, STOCK_NAME, STOCK_KEYWORD) VALUES ( 'us', 'NVDA', '엔비디아');
