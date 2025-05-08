d86f5eedfa87f8ddab6ed1650bd8b084e6fd45bc
Giheon Do <dgh0001@naver.com>
2025-05-08 18:57:27 +0900

    feat: keyword 기반 검색 추가

--- src/main/java/datastreams_knu/bigpicture/news/repository/NewsRepository.java
+++ src/main/java/datastreams_knu/bigpicture/news/repository/NewsRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
     Optional<News> findByKeyword(String keyword);
+
+    List<News> findAllByKeyword(String keyword);
 
     int deleteAllByNewsCrawlingDateBefore(LocalDate newsCrawlingDate);
 }
