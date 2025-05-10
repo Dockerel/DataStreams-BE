b455c96fb468a2fe348eefa651ef2529516e3e04
Giheon Do <dgh0001@naver.com>
2025-05-10 20:43:35 +0900

    add: swagger 엔드포인트 description 추가

--- src/main/java/datastreams_knu/bigpicture/exchange/controller/ExchangeController.java
+++ src/main/java/datastreams_knu/bigpicture/exchange/controller/ExchangeController.java
@@ -3,6 +3,8 @@ package datastreams_knu.bigpicture.exchange.controller;
 import datastreams_knu.bigpicture.common.domain.ApiResponse;
 import datastreams_knu.bigpicture.exchange.controller.dto.ExchangeResponse;
 import datastreams_knu.bigpicture.exchange.service.ExchangeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,13 @@ import java.util.List;
 @RequestMapping("/api/v1/exchanges")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "환율", description = "환율 관련 API")
 public class ExchangeController {
 
     private final ExchangeService exchangeService;
 
     @GetMapping
+    @Operation(summary = "환율 데이터 조회", description = "모든 환율 데이터 조회")
     public ApiResponse<List<ExchangeResponse>> getExchanges() {
         return ApiResponse.ok(exchangeService.getExchanges());
     }
--- src/main/java/datastreams_knu/bigpicture/interest/controller/InterestController.java
+++ src/main/java/datastreams_knu/bigpicture/interest/controller/InterestController.java
@@ -3,6 +3,8 @@ package datastreams_knu.bigpicture.interest.controller;
 import datastreams_knu.bigpicture.common.domain.ApiResponse;
 import datastreams_knu.bigpicture.interest.controller.dto.InterestResponse;
 import datastreams_knu.bigpicture.interest.service.InterestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,19 @@ import java.util.List;
 @RequestMapping("/api/v1/interests")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "금리", description = "금리 관련 API")
 public class InterestController {
 
     private final InterestService interestService;
 
     @GetMapping("/korea")
+    @Operation(summary = "한국 금리 데이터 조회", description = "모든 한국 금리 데이터 조회")
     public ApiResponse<List<InterestResponse>> getKoreaInterests() {
         return ApiResponse.ok(interestService.getKoreaInterests());
     }
 
     @GetMapping("/us")
+    @Operation(summary = "미국 금리 데이터 조회", description = "모든 미국 금리 데이터 조회")
     public ApiResponse<List<InterestResponse>> getUSInterests() {
         return ApiResponse.ok(interestService.getUSInterests());
     }
