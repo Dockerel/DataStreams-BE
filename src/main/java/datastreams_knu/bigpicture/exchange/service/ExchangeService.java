978ab1533090f20c32cd617756a00ecc4476e196
Giheon Do <dgh0001@naver.com>
2025-05-10 14:40:58 +0900

    feat: 환율 데이터 조회 엔드포인트 추가

---
+++ src/main/java/datastreams_knu/bigpicture/exchange/service/ExchangeService.java
@@ -0,0 +1,24 @@
+package datastreams_knu.bigpicture.exchange.service;
+
+import datastreams_knu.bigpicture.exchange.controller.dto.ExchangeResponse;
+import datastreams_knu.bigpicture.exchange.repository.ExchangeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ExchangeService {
+
+    public final ExchangeRepository exchangeRepository;
+
+    public List<ExchangeResponse> getExchanges() {
+        return exchangeRepository.findAll().stream()
+            .map(exchange -> ExchangeResponse.from(exchange))
+            .collect(Collectors.toList());
+    }
+}
