0a2fae4bedd64db7e85a03431a5ce0646236fa39
Giheon Do <dgh0001@naver.com>
2025-05-19 21:02:07 +0900

    feat: stockName validator 추가

---
+++ src/main/java/datastreams_knu/bigpicture/common/util/StockNameValidator.java
@@ -0,0 +1,111 @@
+package datastreams_knu.bigpicture.common.util;
+
+import datastreams_knu.bigpicture.common.dto.DateRangeDto;
+import datastreams_knu.bigpicture.stock.agent.dto.KoreaStockCrawlingDto;
+import datastreams_knu.bigpicture.stock.agent.dto.USStockCrawlingDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Component
+public class StockNameValidator {
+
+    public static final int YEARS_TO_SUBTRACT = 1;
+
+    private final WebClientUtil webClientUtil;
+
+    @Value("${korea-stock.api.base-url}")
+    private String koreaStockBaseUrl;
+    @Value("${korea-stock.api.key}")
+    private String koreaStockApiKey;
+
+    @Value("${us-stock.api.base-url}")
+    private String usStockBaseUrl;
+    @Value("${us-stock.api.key}")
+    private String usStockApiKey;
+
+    public boolean isInvalidStockName(String stockName, String stockType) {
+        return (stockType.equals("korea") && !isExistentKoreaStock(stockName))
+                || (stockType.equals("us") && !isExistentUSStock(stockName));
+    }
+
+    private boolean isExistentKoreaStock(String stockName) {
+        String encodedStockName = encodeString(stockName);
+        DateRangeDto dateRange = getKoreaStockDateRange();
+        String url = createKoreaStockUrl(encodedStockName, dateRange);
+
+        KoreaStockCrawlingDto result = webClientUtil.get(url, KoreaStockCrawlingDto.class);
+
+        return result.getResponse().getBody().getItems().getItem().size() > 0;
+    }
+
+    private boolean isExistentUSStock(String stockName) {
+        DateRangeDto dateRange = getUSStockDateRange();
+        String url = createUSStockUrl(stockName, dateRange);
+
+        System.out.println("url = " + url);
+
+        USStockCrawlingDto response = webClientUtil.get(url, USStockCrawlingDto.class);
+
+        return response.getResults() != null;
+    }
+
+    private String createKoreaStockUrl(String stockName, DateRangeDto dateRange) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(koreaStockBaseUrl);
+        sb.append("?serviceKey=")
+                .append(koreaStockApiKey);
+        sb.append("&itmsNm=")
+                .append(stockName);
+        sb.append("&beginBasDt=")
+                .append(dateRange.getFromDate());
+        sb.append("&endBasDt=")
+                .append(dateRange.getToDate());
+        sb.append("&resultType=json")
+                .append("&numOfRows=1");
+
+        return sb.toString();
+    }
+
+    private String createUSStockUrl(String stockName, DateRangeDto dateRange) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(usStockBaseUrl);
+        sb.append("ticker/").append(stockName);
+        sb.append("/range/1/day")
+                .append("/" + dateRange.getFromDate())
+                .append("/" + dateRange.getToDate());
+        sb.append("?apiKey=")
+                .append(usStockApiKey);
+        sb.append("&limit=1");
+        return sb.toString();
+    }
+
+    private DateRangeDto getKoreaStockDateRange() {
+        LocalDate now = LocalDate.now();
+        LocalDate past = now.minusYears(YEARS_TO_SUBTRACT);
+        String nowDate = parseDate(now);
+        String pastDate = parseDate(past);
+        return DateRangeDto.of(pastDate, nowDate);
+    }
+
+    private DateRangeDto getUSStockDateRange() {
+        LocalDate now = LocalDate.now();
+        LocalDate past = now.minusYears(YEARS_TO_SUBTRACT);
+        String pastDate = past.toString();
+        String nowDate = now.toString();
+        return DateRangeDto.of(pastDate, nowDate);
+    }
+
+    private String parseDate(LocalDate date) {
+        return date.toString().replace("-", "");
+    }
+
+    private String encodeString(String str) {
+        return URLEncoder.encode(str, StandardCharsets.UTF_8);
+    }
+}
