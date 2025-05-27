47132c5224bc69de71cea9e07b0cbb377cefd5dd
Giheon Do <dgh0001@naver.com>
2025-05-27 13:18:28 +0900

    feat: spring, python 서버 테스트용 엔드포인트 추가

---
+++ src/main/java/datastreams_knu/bigpicture/common/controller/TestController.java
@@ -0,0 +1,38 @@
+package datastreams_knu.bigpicture.common.controller;
+
+import datastreams_knu.bigpicture.common.controller.dto.PythonTestDto;
+import datastreams_knu.bigpicture.common.domain.ApiResponse;
+import datastreams_knu.bigpicture.common.util.WebClientUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/test")
+@RestController
+@Tag(name = "테스트", description = "테스트용 API")
+public class TestController {
+
+    private final WebClientUtil webClientUtil;
+
+    @Value("${python.server.url}")
+    private String pythonServerUrl;
+
+    @GetMapping("/spring")
+    @Operation(summary = "테스트 엔드포인트 - spring server", description = "스프링 서버 테스트용 엔드포인트")
+    public ApiResponse<String> getTestSpring() {
+        return ApiResponse.ok("ok");
+    }
+
+    @GetMapping("/python")
+    @Operation(summary = "테스트 엔드포인트 - python server", description = "파이썬 서버 테스트용 엔드포인트")
+    public ApiResponse<String> getTestPython() {
+        PythonTestDto res = webClientUtil.get("http://localhost:8000", PythonTestDto.class);
+        return ApiResponse.ok(res.getStatus());
+    }
+
+}
