53a0f83e0a13d4d0f82e5106e20f80a1f48fe43b
Giheon Do <dgh0001@naver.com>
2025-05-28 20:42:22 +0900

    test: fcm alert repository test

---
+++ src/test/java/datastreams_knu/bigpicture/alert/repository/MemberWatchlistRepositoryTest.java
@@ -0,0 +1,52 @@
+package datastreams_knu.bigpicture.alert.repository;
+
+import datastreams_knu.bigpicture.alert.entity.Member;
+import datastreams_knu.bigpicture.alert.entity.MemberWatchlist;
+import datastreams_knu.bigpicture.alert.service.AlertService;
+import datastreams_knu.bigpicture.alert.service.dto.RegisterWatchlistServiceRequest;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberWatchlistRepositoryTest {
+
+    @Autowired
+    MemberWatchlistRepository memberWatchlistRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    AlertService alertService;
+    @Autowired
+    EntityManager em;
+
+    @Commit
+    @DisplayName("")
+    @Test
+    void test() {
+        String id = "uuid";
+        String fcmToken = "fcmToken";
+        Member member = Member.of(id, fcmToken);
+
+        memberRepository.save(member);
+
+        String stockName = "삼성전자";
+        alertService.registerWatchlist(RegisterWatchlistServiceRequest.of(id, stockName, "korea"));
+
+        alertService.registerWatchlist(RegisterWatchlistServiceRequest.of(id, stockName, "korea"));
+
+        alertService.registerWatchlist(RegisterWatchlistServiceRequest.of(id, "LG디스플레이", "korea"));
+
+        memberWatchlistRepository.deleteByMemberIdAndWatchlistStockName(id, stockName);
+    }
+
+}
