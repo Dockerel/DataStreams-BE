b280b108f239ebcf4ae077116d95ee6cb63833f5
Giheon Do <dgh0001@naver.com>
2025-08-01 20:55:45 +0900

    fix: try executor 로직 수정

--- src/main/java/datastreams_knu/bigpicture/schedule/util/RetryExecutor.java
+++ src/main/java/datastreams_knu/bigpicture/schedule/util/RetryExecutor.java
@@ -8,22 +8,21 @@ import java.util.function.Supplier;
 @Slf4j
 public class RetryExecutor {
 
-    private static int RETRY_COUNT = 3;
+    private final static int RETRY_COUNT = 3;
 
     public static void execute(Supplier<CrawlingResultDto> supplier, String taskName) {
-        try {
-            String message = "";
-            for (int i = 0; i < RETRY_COUNT; i++) {
+        String message = "";
+        for (int i = 0; i < RETRY_COUNT; i++) {
+            try {
                 CrawlingResultDto result = supplier.get();
                 message = result.getMessage();
                 if (result.getResult()) {
                     log.info("{}: {}", taskName, message);
                     return;
                 }
+            } catch (Exception e) {
+                log.warn("{}: {}번째 시도 중 예외 발생 - {}", taskName, i + 1, message);
             }
-            log.error("{}: {}", taskName, message);
-        } catch (Exception e) {
-            log.error("{}: {}", taskName, e.getMessage());
         }
     }
 }
